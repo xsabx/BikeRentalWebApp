c9d62b8d46851b9c0eff8e8614c14dbfd9f22cc7
xsabx <113295583+xsabx@users.noreply.github.com>
2024-08-29 19:17:41 +0000

    basic functioanlity of authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
   config.hosts << "localhost:3000"
 
   config.action_cable.allowed_request_origins = ["https://#{pf_host}", "http://localhost:3000"]
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
